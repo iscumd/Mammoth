d1514b9954f1cba33e33794cc1a860b8565c0415
ISC <isc-robotics@umich.edu>
2019-10-31 20:04:41 -0400

    Now uses pandas to manage waypoint csv's and converts from RPY to Quaternions

--- mammoth_snowplow/src/mammoth_waypoint_publisher.py
+++ mammoth_snowplow/src/mammoth_waypoint_publisher.py
@@ -4,23 +4,43 @@ import rospy
 import std_msgs.msg
 import geometry_msgs.msg
 from tf.transformations import quaternion_from_euler
+import pandas as pd #pandas is used to read in csv files
 
 
-class mammoth_waypoint_manager(object, filename):
-    def __init__(self):
+class mammoth_waypoint_manager(object):
+    def __init__(self, filename):
         self.target_pose = [0,0,0,0,0,0,0] # x,y,z,qx,qy,qz,qw
+        self.waypoint_index = 0
         self.pub = rospy.Publisher('/move_base_simple/goal', geometry_msgs.msg.PoseStamped, queue_size=10)
-        self.import()
+        self.import_file(filename)
         
         
-    def import(self):
+    def import_file(self,filename):
         #import the file here
         #do quaternion_from_euler here
-    
+        print("Importing File")
+        imported = pd.read_csv(filename) #this statement reads in the csv file
+        #note that python will look in the current working directory for this file
+        #print(dataframe) #this should print the dataframe from the csv file 
+        #if you want to skip the first row of the csv file the code is
+        #dataframe = pd.read_csv("filename.csv", skiprows = 1)
+        converted = []
+        
+        for import_counter in range(imported.shape[0]):
+        
+            quaternion = quaternion_from_euler(imported.iloc[import_counter].Roll,imported.iloc[import_counter].Pitch,imported.iloc[import_counter].Yaw)
+            converted.append([imported.iloc[import_counter].X,imported.iloc[import_counter].Y,imported.iloc[import_counter].Z,quaternion[0],quaternion[1],quaternion[2],quaternion[3]])
+
+        self.waypoints = pd.DataFrame(converted,columns=["X","Y","Z","QX","QY","QZ","QW"])
+        print self.waypoints
+        
+        
+
     def check_pose(self):
         #compare pose to target, if its within a certain tolerance move to next
-        
-    def 
+        if self.target_pose == self.latest_pose:
+            self.waypoint_index = self.waypoint_index + 1
+
     
     
     
@@ -55,7 +75,8 @@ class mammoth_waypoint_manager(object, filename):
 if __name__ == '__main__':
     try:
         rospy.init_node('mammoth_waypoint_manager')
-        waypoint_file = rospy.get_param("waypoint_list_filename")
+        #waypoint_file = rospy.get_param("waypoint_list_filename")
+        waypoint_file = "test.csv"
         mammoth = mammoth_waypoint_manager(waypoint_file)
         mammoth.run()
         
