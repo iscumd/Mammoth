1836ed9aa8723286a3bc6e0c35df040a57dbb7f9
ISC <isc-robotics@umich.edu>
2019-11-01 13:55:02 -0400

    Started comparing pose messages

--- mammoth_snowplow/src/mammoth_waypoint_publisher.py
+++ mammoth_snowplow/src/mammoth_waypoint_publisher.py
@@ -9,8 +9,15 @@ import pandas as pd #pandas is used to read in csv files
 
 class mammoth_waypoint_manager(object):
     def __init__(self, filename):
-        self.target_pose = [0,0,0,0,0,0,0] # x,y,z,qx,qy,qz,qw
+        
         self.waypoint_index = 0
+        self.tolerance_x = 0.1
+        self.tolerance_y = 0.1
+        self.tolerance_z = 0.1
+        self.tolerance_qx = 0.1
+        self.tolerance_qy = 0.1
+        self.tolerance_qz = 0.1
+        self.tolerance_qw = 0.1
         self.pub = rospy.Publisher('/move_base_simple/goal', geometry_msgs.msg.PoseStamped, queue_size=10)
         self.import_file(filename)
         
@@ -38,19 +45,31 @@ class mammoth_waypoint_manager(object):
 
     def check_pose(self):
         #compare pose to target, if its within a certain tolerance move to next
-        if self.target_pose == self.latest_pose:
+        good_job = 0
+        if abs(self.latest_pose[0] - self.waypoint.iloc[waypoint_index].X) > self.tolerance_x:
+            if abs(self.latest_pose[1] - self.waypoint.iloc[waypoint_index].Y) > self.tolerance_y:
+                if abs(self.latest_pose[2] - self.waypoint.iloc[waypoint_index].Z) > self.tolerance_z:
+                    if abs(self.latest_pose[3] - self.waypoint.iloc[waypoint_index].QX) > self.tolerance_qx:
+                        if abs(self.latest_pose[4] - self.waypoint.iloc[waypoint_index].QY) > self.tolerance_qy:
+                            if abs(self.latest_pose[5] - self.waypoint.iloc[waypoint_index].QZ) > self.tolerance_qz:
+                                if abs(self.latest_pose[6] - self.waypoint.iloc[waypoint_index].QW) > self.tolerance_qw:
+                                    good_job = 1
+        if good_job:
             self.waypoint_index = self.waypoint_index + 1
+            self.send_it()
 
     
     
     
-    def callback_pose(self, pose):
-        self.latest_pose = pose
+    def callback_pose(self, data):
+        self.latest_pose = data
         self.check_pose()
+        
         
     
     def run(self):
-        rospy.Subscriber("pose", geometry_msgs.msg.PoseStamped, self.callback_pose)
+        #rospy.Subscriber("pose", geometry_msgs.msg.PoseStamped, self.callback_pose)
+        
         rospy.spin()
         
     
@@ -61,13 +80,13 @@ class mammoth_waypoint_manager(object):
         goal = PoseStamped()
         goal.header.frame_id = "base_link"
         goal.header.stamp = rospy.Time.now()
-        goal.pose.position.x = self.target_pose[0]
-        goal.pose.position.y = self.target_pose[1]
-        goal.pose.posiiton.z = self.target_pose[2]
-        goal.pose.orientation.x = self.target_pose[3]
-        goal.pose.orientation.y = self.target_pose[4]
-        goal.pose.orientation.z = self.target_pose[5]
-        goal.pose.orientation.w = self.target_pose[6]
+        goal.pose.position.x = self.waypoints.iloc[self.waypoint_index].X #self.target_pose[0]
+        goal.pose.position.y = self.waypoints.iloc[self.waypoint_index].Y
+        goal.pose.posiiton.z = self.waypoints.iloc[self.waypoint_index].Z
+        goal.pose.orientation.x = self.waypoints.iloc[self.waypoint_index].QX
+        goal.pose.orientation.y = self.waypoints.iloc[self.waypoint_index].QY
+        goal.pose.orientation.z = self.waypoints.iloc[self.waypoint_index].QZ
+        goal.pose.orientation.w = self.waypoints.iloc[self.waypoint_index].QW
         self.pub.publish(goal)
         
     
