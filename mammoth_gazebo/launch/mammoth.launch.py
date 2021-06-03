8bb13e6406127eac95a36b9a67fc10cdd102b232
David Cutting <32181384+dcutting133@users.noreply.github.com>
2021-06-03 18:02:35 -0400

    Clean up the repo and attempt to fix the ros2 build actions (#30)
    * clean up
    
    * add repos file
    
    * remove rosinstall and change build to only use deps

--- mammoth_gazebo/launch/mammoth.launch.py
+++ mammoth_gazebo/launch/mammoth.launch.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2021 Intelligent Systems Club
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -7,17 +29,17 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import EnvironmentVariable
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
-#import xarco
+# import xarco
 
-#xacro_file = os.path.join(urdf_dir, 'test-desc.urdf.xacro')
-#doc = xacro.process_file(xacro_file)
-#robot_desc = doc.toprettyxml(indent='  ')
+# xacro_file = os.path.join(urdf_dir, 'test-desc.urdf.xacro')
+# doc = xacro.process_file(xacro_file)
+# robot_desc = doc.toprettyxml(indent='  ')
 # https://answers.ros.org/question/361623/ros2-robot_state_publisher-xacro-python-launch/
+
 
 def generate_robot_model(pkg_description):
     urdf_dir = os.path.join(pkg_description, 'urdf')
@@ -26,13 +48,16 @@ def generate_robot_model(pkg_description):
         robot_desc = infp.read()
     return robot_desc, urdf_file
 
-def generate_launch_description():
 
+def generate_launch_description():
     # ROS packages
     pkg_mammoth_description = get_package_share_directory('mammoth_description')
     pkg_mammoth_gazebo = get_package_share_directory('mammoth_gazebo')
     pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
     pkg_teleop_twist_joy = get_package_share_directory('teleop_twist_joy')
+
+    # Config
+    joy_config = os.path.join(pkg_mammoth_gazebo, 'config/joystick', 'xbone.config.yaml')
 
     # Launch arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
@@ -104,7 +129,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         arguments=['-d', os.path.join(pkg_mammoth_description, 'rviz', 'mammoth_gazebo.rviz')],
-        condition=IfCondition(LaunchConfiguration('use_rviz'))
+        condition=IfCondition(use_rviz)
     )
 
     joy_with_teleop_twist = IncludeLaunchDescription(
@@ -114,7 +139,7 @@ def generate_launch_description():
         launch_arguments={
             'joy_config': 'xbox',
             'joy_dev': '/dev/input/js0',
-            'config_filepath': os.path.join(pkg_mammoth_gazebo, 'config/joystick', 'xbone.config.yaml')
+            'config_filepath': joy_config
         }.items(),
     )
 
@@ -134,11 +159,10 @@ def generate_launch_description():
     return LaunchDescription([
         # Launch Arguments
         DeclareLaunchArgument('use_sim_time', default_value='true',
-            description='Use simulation (Gazebo) clock if true'
-        ),
+                              description='Use simulation (Gazebo) clock if true'),
+
         DeclareLaunchArgument('use_rviz', default_value='true',
-            description='Open rviz if true'
-        ),
+                              description='Open rviz if true'),
 
         # Nodes
         robot_state_publisher,
