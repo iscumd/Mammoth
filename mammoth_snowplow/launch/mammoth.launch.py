d52a134572e0091642220ad455b8c0ca176027bf
Avery <69600850+girvenavery2022@users.noreply.github.com>
2021-12-03 11:30:18 -0500

    Ros2 snowplow (#39)
    * fixed include tag
    
    * fix remaps for manual control
    
    * unchange controller launch file
    
    * Lutulu Code Updates (#29)
    
    * Clean up the code and added some comments
    
    * whitespace
    
    Co-authored-by: LutuluM <LTMbiye801@gmail.com>
    
    * slam changes
    
    * lidar change
    
    * lidar changes
    
    * changes made
    
    * fixed whitespace
    
    * fixed whitespace pt2
    
    * whitespace fix pt3
    
    * added Pointcloud_to_Laserscan
    
    * fixed capitalization in launch file
    
    * slam up and running
    
    * fixed launch world
    
    * added package to deps
    
    * fixed whitespace:)
    
    * added a repo
    
    * fixed more whitespace
    
    * fixed more whitespace
    
    * hopefully last whitespace
    
    * added ros1 ouster driver to repos
    
    * added filtered topic visualizer
    
    * updated repos files
    
    * attempt to fix vcstool install
    
    * attempt to fix vcstool install
    
    * didnt fix vcstool
    
    * Update deps.repos
    
    * another attempt to fix vcstool
    
    * another attempt to fix vcstool
    
    * another attempt to fix vcstool
    
    * fixed rviz file and updated launch file
    
    * updated laserscan subscription topic
    
    * corrected requested changes:)
    
    * added use sim time launch parameters
    
    * start creating single I world
    
    * initial addition of mammoth snowplow package
    
    * added ros2 ouster driver
    
    * forogt to update changes with main
    
    * some cleanup:)
    
    * fixed a requested change:)
    
    * renamed files and directories using snake_case
    
    * fixed another requested change
    
    * added urdf for physical robot
    
    * moved urdf to mammoth_description
    
    * added license file
    
    * added ros2_ouster to cmake and package
    
    * cleanup
    
    * launch/config architecture clean up
    
    Co-authored-by: nathan <nlemus@umich.edu>
    Co-authored-by: Nathan <63803913+NathanLemus@users.noreply.github.com>
    Co-authored-by: dcutting133 <dcutting@umich.edu>
    Co-authored-by: David Cutting <32181384+dcutting133@users.noreply.github.com>
    Co-authored-by: LutuluM <LTMbiye801@gmail.com>
    Co-authored-by: Avery <agirven@umich.edu>

--- mammoth_snowplow/launch/mammoth.launch.py
+++ mammoth_snowplow/launch/mammoth.launch.py
@@ -1,4 +1,4 @@
-# # MIT License
+# MIT License
 #
 # Copyright (c) 2021 Intelligent Systems Club
 #
@@ -33,144 +33,122 @@ from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
-# import xarco
-
-# xacro_file = os.path.join(urdf_dir, 'test-desc.urdf.xacro')
-# doc = xacro.process_file(xacro_file)
-# robot_desc = doc.toprettyxml(indent='  ')
-# https://answers.ros.org/question/361623/ros2-robot_state_publisher-xacro-python-launch/
-
-
-def generate_robot_model(pkg_description):
-    urdf_dir = os.path.join(pkg_description , 'urdf')
-    urdf_file = os.path.join(urdf_dir, 'mammoth1.urdf')
-    with open(urdf_file, 'r') as infp:
-        robot_desc = infp.read()
-    return robot_desc, urdf_file
-
 
 def generate_launch_description():
     # ROS packages
-    pkg_mammoth_description = get_package_share_directory('mammoth_description')
     pkg_mammoth_snowplow = get_package_share_directory('mammoth_snowplow')
     pkg_teleop_twist_joy = get_package_share_directory('teleop_twist_joy')
 
     # Config
     joy_config = os.path.join(pkg_mammoth_snowplow, 'config/joystick', 'xbone.config.yaml')
-    laserscan_config = os.path.join(pkg_mammoth_snowplow, 'config/Sensors/Lidar', 'pointcloud_to_laserscan.yaml')
-    roboteq_config = os.path.join(pkg_mammoth_snowplow, 'config/Roboteq', 'roboteq.yaml')   
     
     # Launch arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     use_rviz = LaunchConfiguration('use_rviz', default='true')
-    robot_desc, urdf_file = generate_robot_model(pkg_mammoth_description)
 
     # Nodes
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_desc,
-        }]
-    )
-
-    joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        output='screen',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_desc
-        }]
-    )
-
-    pointcloud_to_laserscan = Node(
-        package='pointcloud_to_laserscan',
-        executable='pointcloud_to_laserscan_node',
-        remappings=[
-            ('cloud_in', '/mammoth/filtered_points'),
-            ('scan', '/scan')],
-        parameters=[laserscan_config],
-        name='pointcloud_to_laserscan'
-    )
-
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', os.path.join(pkg_mammoth_description, 'rviz', 'mammoth_gazebo.rviz')],
-        condition=IfCondition(use_rviz)
+    state_publishers = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/state_publishers/state_publishers.launch.py']),
+         launch_arguments={
+            'use_sim_time': use_sim_time
+        }.items(),
     )
 
     joy_with_teleop_twist = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_teleop_twist_joy, 'launch', 'teleop-launch.py')
-        ),
-        launch_arguments={
-            'joy_config': 'xbox',
-            'joy_dev': '/dev/input/js0',
-            'config_filepath': joy_config
+      PythonLaunchDescriptionSource(
+         os.path.join(pkg_teleop_twist_joy, 'launch', 'teleop-launch.py')
+      ),
+      launch_arguments={
+          'joy_config': 'xbox',
+          'joy_dev': '/dev/input/js0',
+          'config_filepath': joy_config
+      }.items(),
+    )
+
+    roboteq = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/roboteq/roboteq.launch.py']),
+         launch_arguments={
+            'use_sim_time': use_sim_time
         }.items(),
     )
 
-    lidar_processor = Node(
-        package='lidar_processor',
-        executable='lidar_processor',
-        name='lidar_processor',
-        output='screen',
-        remappings=[
-            ('/lidar/raw_points', '/mammoth/raw_points'),
-            ('/lidar/filtered_points', '/mammoth/filtered_points'),
-            ('/lidar/unfiltered_points', '/mammoth/unfiltered_points'),
-            ('/lidar/raw_scan', '/mammoth/raw_scan'),
-            ('/lidar/filtered_scan', '/mammoth/filtered_scan'),
-            ('/lidar/unfiltered_scan', '/mammoth/unfiltered_scan'),
-        ],
-        parameters=[{
+    ouster_os1 = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/ouster_os1/os1.launch.py']),
+         launch_arguments={
             'use_sim_time': use_sim_time
-        }]
-    )
-    
-    roboteq = Node(
-        package='isc_roboteq',
-        executable='isc_roboteq',
-        name='isc_roboteq',
-        output='screen',
-        parameters=[roboteq_config],
-    )
-    
-    mammoth_navigation = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_mammoth_snowplow, 'launch/include', 'navigation.launch.py')
-        ),
-        launch_arguments={
+        }.items(), 
+    )   
+
+    realsense = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/realsense/rs_t265_launch.py']),
+         launch_arguments={
+            'use_sim_time': use_sim_time
+        }.items(),
+    )    
+
+    lidar_processor = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/lidar_processor/lidar_processor.launch.py']),
+         launch_arguments={
             'use_sim_time': use_sim_time
         }.items(),
     )
-    
+
+    pointcloud_to_laserscan = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/pointcloud_to_laserscan/pointcloud_to_laserscan.launch.py']),
+         launch_arguments={
+            'use_sim_time': use_sim_time
+        }.items(),
+    )
+
+    navigation = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/navigation/navigation.launch.py']),
+         launch_arguments={
+            'use_sim_time': use_sim_time
+        }.items(),
+    ) 
+
+    rviz = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([os.path.join(
+         pkg_mammoth_snowplow, 'launch'),
+         '/include/rviz/rviz.launch.py']),
+         launch_arguments={
+            'use_rviz': use_rviz,
+            'use_sim_time': use_sim_time
+         }.items(),
+    )
+
     return LaunchDescription([
         # Launch Arguments
-     
         DeclareLaunchArgument('use_sim_time', default_value='false',
-                              description='Use simulation (Gazebo) clock if true'),
+                              description='Use simulation clock if true'),
 
         DeclareLaunchArgument('use_rviz', default_value='true',
                               description='Open rviz if true'),
 
-        DeclareLaunchArgument(name='scanner', default_value='scanner',
-                              description='Namespace for sample topics'),
-
         # Nodes
-        robot_state_publisher,
-        joint_state_publisher,
+        state_publishers,
         joy_with_teleop_twist,
+        roboteq,
+        ouster_os1,
+ 
+        realsense,
         lidar_processor,
         pointcloud_to_laserscan,
-        mammoth_navigation,
-        rviz,
+        navigation,
 
-        roboteq,
+        rviz,
     ])
